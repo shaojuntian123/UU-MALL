7182b895383d1ce4b477a15d8f891c1953afe51f
tsj <1243023168@qq.com>
2020-07-12 16:55:10 +0800

    hello git

--- firstGit/src/main/java/First.java
+++ firstGit/src/main/java/First.java
@@ -4,5 +4,10 @@ public class First {
         System.out.println("hello  git1");
         System.out.println("hello  git2");
         System.out.println("hello  git3");
+        System.out.println("hello  git");
+        System.out.println("hello  git1");
+        System.out.println("hello  git2");
+        System.out.println("hello  git3");
         }
+
 }
